e7ca37ad584e41d7b70253906dcf210e4ff05bd5
GbalsaC <gabrielbalsa@comunidad.unam.mx>
2015-12-01 14:20:23 -0600

    Bundlejs Function wrapping for testing ajax call error.

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -1,6 +1,8 @@
+(function($){
 $(document).ready(function() {
 	
 	$( "h1.logo" ).click(function() {
 	  $( "a.nav-skip" ).text( "Something was selected" ).show().fadeOut( 1000 );
 	});
 });
+})(jQuery);
