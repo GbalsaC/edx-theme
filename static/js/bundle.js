90c131b743e997287a78e692aeb5658b2cd249ad
GbalsaC <gabrielbalsa@comunidad.unam.mx>
2015-12-11 11:02:29 -0600

    Eliminated Jquery from theme and used vendor on platform

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	
 	$( "h1.logo" ).click(function() {
 	  $( "a.nav-skip" ).text( "Something was selected" ).show().fadeOut( 1000 );
+	  console.log('jquery working fine');
 	});
 });
 })()
