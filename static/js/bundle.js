ab5d779ef6fbfcfffadc524e9d06149c9ac0da50
GbalsaC <gabrielbalsa@comunidad.unam.mx>
2016-01-18 10:41:17 -0600

    Reverting to static url directories

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -2,10 +2,7 @@
 	(function(){
 $(document).ready(function() {
 	
-	$( "h1.logo" ).click(function() {
-	  $( "a.nav-skip" ).text( "Something was selected" ).show().fadeOut( 1000 );
-	  console.log('jquery working fine');
-	});
+	$( "footer.global" ).attr('name','Footer from bundlejs on self wrapped anonymus function');
 });
 })()
 }).call(this);
